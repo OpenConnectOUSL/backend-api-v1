2eb98ec0aaafced1c6d13b97ce657a8046b98570
mayura-andrew <mayuraalahakoon@gmail.com>
2024-07-22 00:24:26 +0530

    gh repo clone openconnectousl/oc-backend-api-v1

--- migrations/000003_add_ideas_indexes.down.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS ideas_title_idx;
-DROP INDEX IF EXISTS ideas_genres_idx;
--- migrations/000003_add_ideas_indexes.up.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX IF NOT EXISTS ideas_title_idx ON ideas USING GIN (to_tsvector('simple', title));
-CREATE INDEX IF NOT EXISTS ideas_genres_idx ON ideas USING GIN (tags);
